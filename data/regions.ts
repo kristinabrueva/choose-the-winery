4d576d7af7e5e83be319ad6c81c1431f7701fbe3
Kristina Brueva <kristinabruuu@gmail.com>
2023-06-19 16:14:56 +1000

    Create regions

---
+++ data/regions.ts
@@ -0,0 +1,36 @@
+import { RegionType } from "types";
+
+const REGIONS: RegionType[] = [
+  {
+    name: "McLaren Vale",
+    state: "SA",
+    latitude: -35.22,
+    longtitude: 138.54,
+  },
+  {
+    name: "Margaret River",
+    state: "WA",
+    latitude: -33.96,
+    longtitude: 115.08,
+  },
+  {
+    name: "Mornington",
+    state: "VIC",
+    latitude: -38.22,
+    longtitude: 145.04,
+  },
+  {
+    name: "Coonawarra",
+    state: "VIC",
+    latitude: -37.29,
+    longtitude: 140.83,
+  },
+  {
+    name: "Yarra Valley",
+    state: "VIC",
+    latitude: -37.75,
+    longtitude: 145.1,
+  },
+];
+
+export default REGIONS;
