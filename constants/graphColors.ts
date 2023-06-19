b3b7338e7bf21d0137a555396d9dec51bf8fb833
Kristina Brueva <kristinabruuu@gmail.com>
2023-06-20 08:08:11 +1000

    Extract chart colors as reusable constants

---
+++ constants/graphColors.ts
@@ -0,0 +1,15 @@
+export const BORDER_COLORS = [
+  "rgba(238 130 238)",
+  "rgba(30 130 238)",
+  "rgba(30 30 238)",
+  "rgba(230 30 38)",
+  "rgba(30 130 2)",
+];
+
+export const BACKGROUND_COLORS = [
+  "rgba(238 130 238 / 0.7)",
+  "rgba(30 130 238 / 0.7)",
+  "rgba(30 30 238 / 0.7)",
+  "rgba(230 30 38 / 0.7)",
+  "rgba(30 130 2 / 0.7)",
+];
